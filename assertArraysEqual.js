26bccd03749f66abbc7f4c0ad7c40ddfbe3dd3f7
Khaoula <khaoula.mouanniss@gmail.com>
2020-11-24 19:20:13 +0000

    print if 2 arrays are equals or not

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const eqArrays = function(arr1, arr2){
+  if(arr1.length != arr2.length){
+    return false;
+  }else{
+    for (let i = 0; i < arr1.length; i++){
+      if(arr1[i] !== arr2[i]){
+        return false;
+      }
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function(arr1, arr2){
+  if(eqArrays(arr1, arr2)){
+    console.log(`Assertion Passed: [${arr1}] === [${arr2}].`);
+  }else console.log(`Assertion Failed: [${arr1}] !== [${arr2}].`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);

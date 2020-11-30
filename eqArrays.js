6ec8cacc62c67303c99c3ba960a9c46a4a7c1de5
Khaoula <khaoula.mouanniss@gmail.com>
2020-11-30 17:18:11 +0000

    changes

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if(actual === expected){
-    console.log(`${String.fromCodePoint(55357)}Assertion Passed: [${actual}] === [${expected}].`);
-  }
-  else console.log(`${String.fromCodePoint(0x1F621)}Assertion Failed: [${actual}] !== [${expected}].`);
-};
 const eqArrays = function(arr1, arr2){
   if(arr1.length != arr2.length){
     return false;
@@ -17,9 +11,4 @@ const eqArrays = function(arr1, arr2){
   return true;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+module.exports = eqArrays;
--- middle.js
+++ middle.js
@@ -1,16 +1,3 @@
-const eqArrays = function(arr1, arr2){
-  for (let i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function(arr1, arr2){
-  if(eqArrays(arr1, arr2)){
-    console.log(`Assertion Passed: [${arr1}] === [${arr2}].`);
-  }else console.log(`Assertion Failed: [${arr1}] !== [${arr2}].`);
-};
 const isOdd = function (num){
   return (num % 2 != 0);
 };
@@ -23,15 +10,12 @@ const middle = function(arr){
     let midIndex = Math.floor(arr.length/2) - 1;
     if (isOdd(arr.length)){
       res = arr[midIndex + 1];
-    }else{
+    } else {
       res.push(arr[midIndex]);
       res.push(arr[midIndex + 1]);
     }
   }
   return res;
 };
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+module.exports = middle;

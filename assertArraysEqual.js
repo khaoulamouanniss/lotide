bbb51ffe2adbca349e07b1cabfe12884fb37b615
Khaoula <khaoula.mouanniss@gmail.com>
2020-11-30 15:05:17 +0000

    Update lotide

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,7 +16,7 @@ const assertArraysEqual = function(arr1, arr2){
   }else console.log(`Assertion Failed: [${arr1}] !== [${arr2}].`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1], [1]);
 assertArraysEqual([1, 2, 3], [3, 2, 1]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
--- head.js
+++ head.js
@@ -1,12 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if(actual === expected){
-    console.log(`${String.fromCodePoint(55357)}Assertion Passed: [${actual}] === [${expected}].`);
-  }
-  else console.log(`${String.fromCodePoint(0x1F621)}Assertion Failed: [${actual}] !== [${expected}].`);
-};
-
 const head = function(arr){
   return arr[0];
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head([]), "Hello");
+
+
+module.exports = head;
